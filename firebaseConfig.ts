34ae7959c8a4083a41dfda7e646a51fbc8a89479
Leirote <leiroos@hotmail.com>
2026-02-26 16:26:09 -0300

    atualziação de nome de hosting

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -5,12 +5,12 @@ import { getAnalytics } from "firebase/analytics";
 
 // Configuração do Firebase fornecida pelo usuário
 const firebaseConfig = {
-  apiKey: "AIzaSyDNsDMCkcIB6EW_ZDKa_ucUxjyQHJ9peZI",
-  authDomain: "entregasnatture.firebaseapp.com",
-  projectId: "entregasnatture",
-  storageBucket: "entregasnatture.firebasestorage.app",
-  messagingSenderId: "13398196402",
-  appId: "1:13398196402:web:84a585bbe02c551b5e49ab",
+  apiKey: "AIzaSyDNsDMCkcIB6EW_ZDKa_ucUxjyQHJ9peZI", // ATENÇÃO: Verifique se a apiKey mudou no novo projeto!
+  authDomain: "nattureentregas.firebaseapp.com",
+  projectId: "nattureentregas",
+  storageBucket: "nattureentregas.firebasestorage.app",
+  messagingSenderId: "13398196402", // ATENÇÃO: Verifique se mudou!
+  appId: "1:13398196402:web:84a585bbe02c551b5e49ab", // ATENÇÃO: Verifique se mudou!
   measurementId: "G-27TZHM75KN"
 };
 
